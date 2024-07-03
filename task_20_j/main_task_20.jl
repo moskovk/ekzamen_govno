581858b9a0c2d96632b1aa13c8b0e62c2ea866e0
Margaret <margaritamoskovka@gmail.com>
2024-07-03 21:13:14 +0300

    девочки это зачет

--- task_20_j/main_task_20.jl
+++ task_20_j/main_task_20.jl
@@ -42,44 +42,6 @@ using Random
 using Statistics
 using BenchmarkTools
 
-# Функция для выполнения быстрой сортировки (сортировка Хоара)
-function quicksort!(arr::Vector{T}, low::Int, high::Int) where T
-    if low < high
-        # Получаем индекс элемента-разделителя
-        p = partition!(arr, low, high)
-        # Рекурсивно сортируем левую и правую части массива
-        quicksort!(arr, low, p - 1)
-        quicksort!(arr, p + 1, high)
-    end
-end
-
-# Функция для разделения массива
-function partition!(arr::Vector{T}, low::Int, high::Int) where T
-    pivot = arr[high]
-    i = low - 1
-
-    for j in low:high-1
-        if arr[j] <= pivot
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-        end
-    end
-
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-end
-
-# Вспомогательная функция для удобного вызова быстрой сортировки
-function quicksort!(arr::Vector{T}) where T
-    quicksort!(arr, 1, length(arr))
-end
-
-# Тестирование функции quicksort!
-arr = [3, 6, 8, 10, 1, 2, 1]
-println("Исходный массив: ", arr)
-quicksort!(arr)
-println("Отсортированный массив: ", arr)
-
 # Генерация случайного массива заданной длины
 function generate_random_array(length::Int, max_value::Int = 1000)
     return rand(1:max_value, length)
--- task_26_j/main_task_26.jl
+++ task_26_j/main_task_26.jl
@@ -4,89 +4,52 @@ struct Vector_2D{T}
     y::T
 end
 
-# Конструктор для создания вектора
+# Конструктор для создания объекта Vector_2D
 function Vector_2D(x::T, y::T) where T
     return Vector_2D{T}(x, y)
 end
 
-# Перегрузка операции сложения
-function +(v1::Vector_2D{T}, v2::Vector_2D{T}) where T
-    return Vector_2D(v1.x + v2.x, v1.y + v2.y)
-end
+# Перегрузка оператора сложения для Vector_2D
+Base.:+(v1::Vector_2D{T}, v2::Vector_2D{T}) where T = Vector_2D(v1.x + v2.x, v1.y + v2.y)
 
-# Перегрузка операции вычитания
-function -(v1::Vector_2D{T}, v2::Vector_2D{T}) where T
-    return Vector_2D(v1.x - v2.x, v1.y - v2.y)
-end
+# Перегрузка оператора вычитания для Vector_2D
+Base.:-(v1::Vector_2D{T}, v2::Vector_2D{T}) where T = Vector_2D(v1.x - v2.x, v1.y - v2.y)
 
-# Перегрузка операции умножения на скаляр
-function *(v::Vector_2D{T}, scalar::T) where T
-    return Vector_2D(v.x * scalar, v.y * scalar)
-end
+# Перегрузка оператора умножения на скаляр для Vector_2D
+Base.:*(v::Vector_2D{T}, scalar::S) where {T, S} = Vector_2D(v.x * scalar, v.y * scalar)
+Base.:*(scalar::S, v::Vector_2D{T}) where {T, S} = Vector_2D(v.x * scalar, v.y * scalar)
 
-function *(scalar::T, v::Vector_2D{T}) where T
-    return Vector_2D(v.x * scalar, v.y * scalar)
-end
-
-# Перегрузка операции деления на скаляр
-function /(v::Vector_2D{T}, scalar::T) where T
-    return Vector_2D(v.x / scalar, v.y / scalar)
-end
-
-# Вычисление длины (модуля) вектора
+# Функция для вычисления длины вектора
 function length(v::Vector_2D{T}) where T
     return sqrt(v.x^2 + v.y^2)
 end
 
-# Нормализация вектора
-function normalize(v::Vector_2D{T}) where T
-    len = length(v)
-    if len == 0
-        throw(DomainError("Невозможно нормализовать нулевой вектор"))
-    end
-    return v / len
-end
-
-# Скалярное произведение двух векторов
+# Функция для вычисления скалярного произведения двух векторов
 function dot(v1::Vector_2D{T}, v2::Vector_2D{T}) where T
     return v1.x * v2.x + v1.y * v2.y
 end
 
-# Векторное произведение двух векторов (по модулю в 2D)
-function cross(v1::Vector_2D{T}, v2::Vector_2D{T}) where T
-    return v1.x * v2.y - v1.y * v2.x
-end
-
-# Угол между двумя векторами
+# Функция для вычисления угла между двумя векторами
 function angle_between(v1::Vector_2D{T}, v2::Vector_2D{T}) where T
-    dot_product = dot(v1, v2)
-    lengths_product = length(v1) * length(v2)
-    if lengths_product == 0
-        throw(DomainError("Невозможно вычислить угол с нулевым вектором"))
-    end
-    cos_theta = dot_product / lengths_product
-    return acos(clamp(cos_theta, -1, 1))  # Угол в радианах
+    return acos(dot(v1, v2) / (length(v1) * length(v2)))
 end
 
-# Перегрузка функции show для красивого вывода
-Base.show(io::IO, v::Vector_2D{T}) where T = print(io, "Vector_2D(", v.x, ", ", v.y, ")")
+# Функция для вычисления нормального вектора (поворот на 90 градусов)
+function normal_vector(v::Vector_2D{T}) where T
+    return Vector_2D(-v.y, v.x)
+end
 
-# Тестирование функций и операций
+# Тестирование операций и функций для Vector_2D
 v1 = Vector_2D(3.0, 4.0)
 v2 = Vector_2D(1.0, 2.0)
 
 println("v1: ", v1)
 println("v2: ", v2)
-
 println("v1 + v2: ", v1 + v2)
 println("v1 - v2: ", v1 - v2)
 println("v1 * 2: ", v1 * 2)
 println("2 * v2: ", 2 * v2)
-println("v1 / 2: ", v1 / 2)
-
 println("Длина v1: ", length(v1))
-println("Нормализованный v1: ", normalize(v1))
-
 println("Скалярное произведение v1 и v2: ", dot(v1, v2))
-println("Векторное произведение v1 и v2 (по модулю): ", cross(v1, v2))
-println("Угол между v1 и v2 (в радианах): ", angle_between(v1, v2))
+println("Угол между v1 и v2: ", angle_between(v1, v2))
+println("Нормальный вектор к v1: ", normal_vector(v1))
