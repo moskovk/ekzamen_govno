5332778bfe47ce4e374b91ea48712432923c475e
Margaret <margaritamoskovka@gmail.com>
2024-07-03 18:36:17 +0300

    девочки это капец

--- task_20_j/main_task_20.jl
+++ task_20_j/main_task_20.jl
@@ -1,29 +1,28 @@
-# Функция для разделения массива и определения опорного элемента
+# Функция для выполнения быстрой сортировки (сортировка Хоара)
+function quicksort!(arr::Vector{T}, low::Int, high::Int) where T
+    if low < high
+        # Получаем индекс элемента-разделителя
+        p = partition!(arr, low, high)
+        # Рекурсивно сортируем левую и правую части массива
+        quicksort!(arr, low, p - 1)
+        quicksort!(arr, p + 1, high)
+    end
+end
+
+# Функция для разделения массива
 function partition!(arr::Vector{T}, low::Int, high::Int) where T
-    pivot = arr[high]  # Опорный элемент
-    i = low - 1        # Индекс меньшего элемента
+    pivot = arr[high]
+    i = low - 1
 
     for j in low:high-1
         if arr[j] <= pivot
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]  # Обмен элементов
+            arr[i], arr[j] = arr[j], arr[i]
         end
     end
 
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]  # Обмен опорного элемента
-    return i + 1  # Возвращаем индекс опорного элемента
-end
-
-# Основная функция быстрой сортировки
-function quicksort!(arr::Vector{T}, low::Int, high::Int) where T
-    if low < high
-        # Получаем индекс опорного элемента, с которым массив разделен
-        pi = partition!(arr, low, high)
-
-        # Рекурсивно сортируем элементы до и после опорного элемента
-        quicksort!(arr, low, pi - 1)
-        quicksort!(arr, pi + 1, high)
-    end
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 end
 
 # Вспомогательная функция для удобного вызова быстрой сортировки
@@ -31,35 +30,77 @@ function quicksort!(arr::Vector{T}) where T
     quicksort!(arr, 1, length(arr))
 end
 
-# Тестирование функции быстрой сортировки
-arr = [10, 7, 8, 9, 1, 5]
+# Тестирование функции quicksort!
+arr = [3, 6, 8, 10, 1, 2, 1]
 println("Исходный массив: ", arr)
 quicksort!(arr)
 println("Отсортированный массив: ", arr)
 
-using Random  # Для генерации случайных чисел
-using Statistics  # Для вычисления статистических данных
-using BenchmarkTools  # Для измерения производительности
+using Pkg
+Pkg.add("BenchmarkTools")
+using Random
+using Statistics
+using BenchmarkTools
 
-# Функция для генерации случайного массива заданной длины
-function generate_random_array(n::Int)
-    return rand(1:1000, n)
-end
-
-# Функция для выполнения экспериментов
-function experiment()
-    sizes = [100, 1000, 5000, 10000, 20000]  # Размеры массивов для тестирования
-    results = Dict{Int, Float64}()
-
-    for size in sizes
-        arr = generate_random_array(size)
-        @show size  # Выводим размер массива
-        @btime quicksort!(arr) setup = (arr = copy(arr)) evals = 1  # Измеряем время сортировки и выводим результаты
+# Функция для выполнения быстрой сортировки (сортировка Хоара)
+function quicksort!(arr::Vector{T}, low::Int, high::Int) where T
+    if low < high
+        # Получаем индекс элемента-разделителя
+        p = partition!(arr, low, high)
+        # Рекурсивно сортируем левую и правую части массива
+        quicksort!(arr, low, p - 1)
+        quicksort!(arr, p + 1, high)
     end
 end
 
-# Проведение экспериментов
-experiment()
+# Функция для разделения массива
+function partition!(arr::Vector{T}, low::Int, high::Int) where T
+    pivot = arr[high]
+    i = low - 1
 
-using Pkg
-Pkg.add("BenchmarkTools")
+    for j in low:high-1
+        if arr[j] <= pivot
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+        end
+    end
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+end
+
+# Вспомогательная функция для удобного вызова быстрой сортировки
+function quicksort!(arr::Vector{T}) where T
+    quicksort!(arr, 1, length(arr))
+end
+
+# Тестирование функции quicksort!
+arr = [3, 6, 8, 10, 1, 2, 1]
+println("Исходный массив: ", arr)
+quicksort!(arr)
+println("Отсортированный массив: ", arr)
+
+# Генерация случайного массива заданной длины
+function generate_random_array(length::Int, max_value::Int = 1000)
+    return rand(1:max_value, length)
+end
+
+# Проведение эксперимента с сортировкой Хоара на массивах разных размеров
+function experimental_study()
+    sizes = [100, 1000, 5000, 10000, 20000]
+    results = Dict()
+
+    for size in sizes
+        arr = generate_random_array(size)
+        time = @belapsed quicksort!(arr)
+        results[size] = time
+    end
+
+    return results
+end
+
+# Запуск экспериментального исследования и вывод результатов
+results = experimental_study()
+for (size, time) in results
+    println("Размер массива: $size, время сортировки: $time секунд")
+end
